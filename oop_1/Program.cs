d93c6a61c1f12ba6e0f0b7cd6a68baf9720c495c
fca124419 <fca124419@stud.gibb.ch>
2023-08-31 18:34:56 +0200

    Projektdateien hinzufügen.

---
+++ oop_1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace oop_1
+{
+    using static System.Math;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bitte Nummer eingeben");
+            int inpurNumber = int.Parse(Console.ReadLine());
+            int squareNumber = (int)Sqrt(inpurNumber);
+            Console.WriteLine("Die Wurzel von " + inpurNumber + " ist: " + squareNumber);
+
+
+            
+        }
+    }
+}
